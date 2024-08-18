a48b3df2957ed97bb93512281ce3166baf5b45f4
BhaargavGupta P <krishna.bhaargav@gmail.com>
2024-08-18 12:00:43 +0530

    .

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -1,48 +1,13 @@
-// import React from 'react'
-<<<<<<< HEAD
-=======
-// import deptImg from '../Assets/deptImg.png'
->>>>>>> 762d3c89e20e966b5648bb525ca87b8a9c7c0231
 
-// const about = () => {
-//   return (
-//     <div className='parent'>
-<<<<<<< HEAD
-//       Sairam College's CSE department delivers a forward-thinking education with a focus on practical experience and industry connections. The program features modern labs and a diverse faculty, ensuring students gain both theoretical knowledge and hands-on skills. 
-=======
-//       <div>
-//         <div id='img-holder'>
-//           <img src={deptImg} alt="CSE Dept Logo" />
-//         </div>
-//       </div> 
->>>>>>> 762d3c89e20e966b5648bb525ca87b8a9c7c0231
-//     </div>
-//   )
-// }
-
-// export default about
 import React from 'react'
-<<<<<<< HEAD
-import deptImg from '../Assets/deptImg.png'
-import aboutBG from '../Assets/aboutBG.png'
-import deptLogo from '../Assets/deptLogo.png'
-import '../styles/aboutPG.css'
-=======
 import aboutBG from '../Assets/aboutBG.png'
 import deptLogo from '../Assets/deptLogo.png'
 import '../styles/aboutPg.css'
->>>>>>> 762d3c89e20e966b5648bb525ca87b8a9c7c0231
 
 const About = () => {
   return (
     <div className='parent' style={{ backgroundImage: `url(${aboutBG})` }}>
       <div className='content-wrapper'>
-<<<<<<< HEAD
-        <div className='img-holder'>
-          <img src={deptImg} alt="CSE Dept Logo" />
-        </div>
-=======
->>>>>>> 762d3c89e20e966b5648bb525ca87b8a9c7c0231
         <div className='logo-holder'>
           <img src={deptLogo} alt="Dept Logo" />
         </div>
@@ -55,8 +20,5 @@ const About = () => {
 }
 
 export default About
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 762d3c89e20e966b5648bb525ca87b8a9c7c0231
