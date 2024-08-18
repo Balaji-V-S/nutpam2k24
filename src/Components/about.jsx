d7918f6be448f30c3d542424d7d0358509232d0f
abilash-mosuru <134186811+abilash-mosuru@users.noreply.github.com>
2024-08-18 11:19:34 +0530

    Update about.jsx

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -14,19 +14,31 @@
 // }
 
 // export default about
+// import React from 'react'
+// import deptImg from '../Assets/deptImg.png'
+
+// const about = () => {
+//   return (
+//     <div className='parent'>
+//       <div>
+//         <div id='img-holder'>
+//           <img src={deptImg} alt="CSE Dept Logo" />
+//         </div>
+//       </div> 
+//     </div>
+//   )
+// }
+
+// export default about
 import React from 'react'
-import deptImg from '../Assets/deptImg.png'
 import aboutBG from '../Assets/aboutBG.png'
 import deptLogo from '../Assets/deptLogo.png'
-import '../styles/aboutPG.css'
+import '../styles/aboutPg.css'
 
 const About = () => {
   return (
     <div className='parent' style={{ backgroundImage: `url(${aboutBG})` }}>
       <div className='content-wrapper'>
-        <div className='img-holder'>
-          <img src={deptImg} alt="CSE Dept Logo" />
-        </div>
         <div className='logo-holder'>
           <img src={deptLogo} alt="Dept Logo" />
         </div>
@@ -37,5 +49,8 @@ const About = () => {
     </div>
   )
 }
+
+export default About
+
 
 export default About
