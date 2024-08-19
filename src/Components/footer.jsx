bd96e5b316c0ff7c13ab71e7cac80c353ae2629d
Balaji-V-S <vsbrobot@gmail.com>
2024-08-19 19:04:36 +0530

    Abt section fixes

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -36,6 +36,7 @@ const footer = () => {
     <p className='co-name'>Abishek M- 9789637083</p>
     <p className='co-name'>Aiswarya S M -9790867582</p>
   </div>
+  
   <div class="map-container">
   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.1749831235406!2d80.05065757358885!3d12.960652315121195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52f51f638ddfbb%3A0xf3aef7ec7c8979ba!2sSri%20Sairam%20Institute%20of%20Technology!5e0!3m2!1sen!2sin!4v1723958415556!5m2!1sen!2sin"   allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </div>
