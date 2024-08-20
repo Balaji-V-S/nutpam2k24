67f2e2946bf21b50f80bdfcd372a0af29d32d706
BhaargavGupta P <krishna.bhaargav@gmail.com>
2024-08-20 22:47:16 +0530

    final responsive commit

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -17,7 +17,7 @@ const footer = () => {
       </div>
       <div className="mail">
         <img src={mail} className="mail-logo" alt="Mail Logo"/>
-        <h4 className="mail-id">nutpamsairamit.edu.in</h4>
+        <h4 className="mail-id">nutpam<span>&#64;</span>sairamit.edu.in</h4>
       </div>
       <div className="contact">
         <a href="https://www.instagram.com/nutpam_2024"><img src={insta} className="instagram-logo" alt="Instagram"/></a>
@@ -27,13 +27,18 @@ const footer = () => {
     </div>
     
     <div className="coordinators-det">
+      <div className='co-staff'>
       <h6 className='co-heading'>Staff Coordinator</h6>
       <h6 className='co-name'>Mr. J.Pathmanaban</h6>
       <h6 className='co-name'>Ms. S.Mathupriya</h6>
+      </div>
+      <div>
       <h6 className='co-heading'>Student Coordinator</h6>
       <h6 className='co-name'>Abishek M- 9789637083</h6>
       <h6 className='co-name'>Aiswarya S M -9790867582</h6>
-    </div>
+    
+      </div>
+      </div>
     
     <div class="map-container">
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.1749831235406!2d80.05065757358885!3d12.960652315121195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52f51f638ddfbb%3A0xf3aef7ec7c8979ba!2sSri%20Sairam%20Institute%20of%20Technology!5e0!3m2!1sen!2sin!4v1723958415556!5m2!1sen!2sin"   allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
