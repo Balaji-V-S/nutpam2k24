a728a35310c45f0ae04aa08fc6f12e85fe77571d
N.Bharathraj <bharathrajnarashiman@gmail.com>
2024-08-24 11:23:28 +0530

    navigation error corrected

--- src/App.js
+++ src/App.js
@@ -19,17 +19,19 @@
 
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Routes, Route } from 'react-router-dom'; 
-import Home from './Pages/homePg'
+import Home from './Pages/homePg';
+
+import About from './Components/about';
+
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} /> */}
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/contact" element={<Contact />} /> */}
+        <Route path="*" element={<Home/>} />
       </Routes>
     </div>
   );
